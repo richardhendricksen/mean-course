164fee9dbcae6c8d8d37edaf3c489c4203d9a4f1
richardhendricksen <richard.hendricksen@gmail.com>
2019-04-07 15:29:04 +0200

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27019/node-angular')
+  .then(() => {
+    console.log('connected to database');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+   'Origin, X-Requested-With, Content-Type, Accept');
+   res.setHeader('Access-Control-Allow-Methods',
+   'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
+
+module.exports = app;
---
+++ backend/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const User = require('../models/user')
+
+const router = express.Router();
+
+router.post('/signup', (req, res, next) => {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+});
+
+module.exports = router;
